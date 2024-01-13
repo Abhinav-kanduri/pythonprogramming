e158e2f9aeb9304cc87973b41b17dffc9959d98e
Abhinav Kanduri <abhinavkanduri92.ds@gmail.com>
2024-01-13 17:55:05 -0600

    comments

--- reverse-string.py
+++ reverse-string.py
@@ -1,1 +1,12 @@
-def 
+#define a function reversestring
+def reversestring(x):
+    #assigning a new variable to the empty string 
+    x2 = "" 
+    #using range with step 1 on reverse direction
+    for i in range(len(x)-1,-1,-1):
+        #interating the loop using i value
+        x2 += x[i]
+        #print new string value 
+    print(x2)
+reversestring("abhinav")
+
